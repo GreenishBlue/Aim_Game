d1187b5c4a845650244d57c60477d68f43d789d3
Denver Brierley <denver.brierley@gmail.com>
2020-05-22 05:49:41 +0100

    Create screens

---
+++ Aim_Game.py
@@ -0,0 +1,23 @@
+#import turtle #import turtle for main game
+import tkinter #import tkinter for UI
+
+def menuScreen():
+    #create menu screen
+    window = tkinter.Tk()
+    window.title("Aim Game")
+    window.geometry ("800x800")
+
+    window.mainloop()
+
+def mainGame() :
+    #create main game screen
+    wn = turtle.Screen()
+    wn.title ("Aim Game")
+    wn.bgcolor ("grey")
+    wn.setup (width=800, height= 800)
+
+
+    wn.mainloop()
+
+#calls menu function
+menuScreen()
