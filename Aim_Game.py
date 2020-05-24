a7e9efa0f37633ec4f7778d9cc64f1f2996f8d31
Denver Brierley <denver.brierley@gmail.com>
2020-05-24 02:54:53 +0100

    Added buttons, labels and some menu functions

--- Aim_Game.py
+++ Aim_Game.py
@@ -1,16 +1,29 @@
-import turtle #import turtle for main game
-import tkinter #import tkinter for UI
+import turtle 
+import tkinter 
 
 def menuScreen():
-    #create menu screen
     window = tkinter.Tk()
     window.title("Aim Game")
     window.geometry ("800x800")
+    window.configure(bg="teal")
+    
+    title = tkinter.Label(window, text="AIM GAME", font="Impact 60", bg="Teal", fg="brown")
+    title.pack()
+
+    playButton = tkinter.Button(window, text="PLAY!", width="100", font="20", bg="teal", fg="white", command=mainGame)
+    playButton.pack()
+
+    settingsButton = tkinter.Button(window, text="Settings", width="100", font="20", bg="teal", fg="white")
+    settingsButton.pack()
+
+    exitButton = tkinter.Button(window, text="Exit Game", width="100", font="20", bg="teal", fg="white", command=window.destroy)
+    exitButton.pack()
+
 
     window.mainloop()
 
 def mainGame() :
-    #create main game screen
+
     wn = turtle.Screen()
     wn.title ("Aim Game")
     wn.bgcolor ("grey")
@@ -19,5 +32,5 @@ def mainGame() :
 
     wn.mainloop()
 
-#calls menu function
+
 menuScreen()
